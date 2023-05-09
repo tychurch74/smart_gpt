2971e472bc83f2c8bd6d8802842a7b3a8cac3944
Tyler Church <tychurch74@gmail.com>
2023-05-09 11:03:23 -0400

    refactor: clean chain of thought and main

--- smart_gpt/__main__.py
+++ smart_gpt/__main__.py
@@ -1,6 +1,34 @@
-"""Entry point for python_project_template."""
+"""Entry point for smart_gpt implementation."""
 
-from .cli import main  # pragma: no cover
+import datetime
+from chain_of_thought import chain_of_thought_response
 
-if __name__ == "__main__":  # pragma: no cover
+
+def main():
+    """Main function.
+    
+    Change the initial_guidance and eval_metrics variables to change the type of response.
+    
+    Date and time are printed to the console to show how long the response takes to generate, purely for testing purposes.
+
+    Refer to gpt_agents.py for more information about the chain of prompts used.
+    
+    """
+    initial_guidance = "factual, logical, and clear"
+    eval_metrics = "factual accuracy, logical consistency, and clarity"
+   
+    user_input = input("Enter your question: ")
+    
+    print("Start time: ")
+    print(datetime.datetime.now())
+    print("\n\n")
+    
+    response = chain_of_thought_response(user_input, initial_guidance, eval_metrics)
+    
+    print(f"final response: {response}\n\n")
+    print("End time: ")
+    print(datetime.datetime.now())
+
+
+if __name__ == "__main__":  
     main()
--- smart_gpt/chain_of_thought.py
+++ smart_gpt/chain_of_thought.py
@@ -1,5 +1,6 @@
 import openai
-import os
+import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 # Function to generate chatbot response
@@ -17,18 +18,22 @@ def generate_chatbot_response(
 
     return response["choices"][0]["message"]["content"]
 
-
 def chain_of_thought_response(user_input, initial_guidance, eval_metrics):
     # Generate response options
     ai_system_msg = f"You are an AI language model trained by OpenAI to assist users with answering various questions or requests. Your responses should be {initial_guidance}. If you are unsure about some of the information in your response, preface that information by saying you are not totally sure."
     preface_message = f"Question: {user_input} Answer: Let's work this out in a step by step way to be sure we have the right answer."
-    response_options = [
-        generate_chatbot_response(ai_system_msg, preface_message) for _ in range(3)
-    ]
+
+    with ThreadPoolExecutor() as executor:
+        # Submit the API calls concurrently
+        futures = [executor.submit(generate_chatbot_response, ai_system_msg, preface_message) for _ in range(3)]
+
+        # Collect the results as they complete
+        response_options = [future.result() for future in as_completed(futures)]
+
     formatted_options = ", ".join(
         f"Option {i + 1}: {response_options[i]}" for i in range(3)
     )
-
+    
     # Generate researcher evaluation
     ai_researcher_msg = f"You are an AI language model trained by OpenAI to act as a researcher tasked with evaluating the quality of responses to a user's prompt based on the following metrics: {eval_metrics}. Be sure to include your reasoning for each of your evaluations of the given responses."
     researcher_prompt = f"Original prompt: {user_input} Response options: {formatted_options}. You are a researcher tasked with evaluating the quality of these response options. List any flaws or faulty logic of each response. Let's think about this step by step:"
@@ -37,17 +42,9 @@ def chain_of_thought_response(user_input, initial_guidance, eval_metrics):
     )
 
     # Find the best response and improve it
-    ai_decision_msg = f"You are an AI language model trained by OpenAI to decide which response option a research evaluator thought was best and then improve the quality of the chosen response based on the following metrics: {eval_metrics} and finally print out your final improved response."
-    final_response_prompt = f"Find which of the following responses the researcher thought was best, improve that response if necessary and respond only with your final improved response. You do not need to show your reasoning, just print out your final improved response: original responses: {formatted_options}researcher evalutions: {researcher_evaluation}"
+    ai_decision_msg = f"You are an AI language model trained by OpenAI to decide which response option a research evaluator thought was best and then improve the quality of the chosen response based on the following metrics: {eval_metrics} and finally print out your final improved response for the user to see."
+    final_response_prompt = f"Find which of the following responses the researcher thought was best, and improve that response to be used as a final response to a user's question or request. original responses: {formatted_options} researcher evalutions: {researcher_evaluation}"
     final_response = generate_chatbot_response(ai_decision_msg, final_response_prompt)
 
     return final_response
 
-
-# Example usage
-initial_guidance = "factual, logical, and clear"
-eval_metrics = "factual accuracy, logical consistency, and clarity"
-user_input = input("Enter your question: ")
-print("\n\n")
-response = chain_of_thought_response(user_input, initial_guidance, eval_metrics)
-print(f"final response: {response}\n\n")
